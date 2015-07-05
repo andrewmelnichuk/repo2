2bcce6e2ac3fcf8ff6b026e6cde5e727e88a8d8c
andrew.melnichuk <andrew.melnichuk@gmail.com>
2015-07-06 00:27:49 +0300

    Minor

--- client/Program.cs
+++ client/Program.cs
@@ -5,6 +5,7 @@ public class Program
   public static void Main(string[] args)
   {
     Channel.On<TestMessage>(m => Console.WriteLine(m.Id));
+    Channel.Open();
     
     Console.WriteLine("hello client");
   }
