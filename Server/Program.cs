19a181883e7e80138b25fbbced335a500321e44e
andrewmelnichuk <andrew.melnichuk@gmail.com>
2015-07-06 18:23:48 +0300

    Added MVC 5 sample app

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,7 +4,11 @@ public class Program
 {
   public static void Main(string[] args)
   {
-    Console.WriteLine("Hello world!!!");
+    Console.WriteLine("--- Server ---");
+    
+    Channel.On<TestMessage>(m => Console.WriteLine(m.Id));
+    
+    
     Console.WriteLine("Press any key to exit...");
     Console.ReadLine();
   }
--- client/Program.cs
+++ client/Program.cs
@@ -4,6 +4,8 @@ public class Program
 {
   public static void Main(string[] args)
   {
+    Console.WriteLine("--- Client ---");
+    
     Channel.On<TestMessage>(m => Console.WriteLine(m.Id));
     var channel = new Channel();
     channel.Open();
