fbb290409ebe8b06dc0db44624b9465981a597e4
andrewmelnichuk <andrew.melnichuk@gmail.com>
2015-07-15 18:21:34 +0300

    Revision added.

--- Web/Server/Common/Entity.cs
+++ Web/Server/Common/Entity.cs
@@ -3,6 +3,8 @@ namespace Server.Common
   public class Entity
   {
     public int Id;
+    public int Revision;    
+    
     public Entity Clone()
     {
       return (Entity) MemberwiseClone();
