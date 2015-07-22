19f1be6bc9352572223da6c6fb38024004d445c0
andrew.melnichuk <andrew.melnichuk@gmail.com>
2015-07-22 23:23:13 +0300

    Improve revisions issuing

--- Web/Server/Common/Data/Revision.cs
+++ Web/Server/Common/Data/Revision.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Threading;
 
@@ -8,43 +7,43 @@ namespace Server.Common.Data
   {
     private const string _revFilePath = "./Data/Rev";
     private const int _revCount = 100;
-    
+
     private static long _maxRev;
     private static long _curRev;
-    
+
     static Revision()
     {
-      ReadCurRev();
+      _curRev = ReadRev();
       _maxRev = _curRev + _revCount;
-      WriteMaxRev();
+      WriteRev(_maxRev);
     }
-    
+
     // MUST be called under lock
     public static long Next(ReaderWriterLockSlim locker)
     {
-      if (!locker.IsWriteLockHeld)
-        throw new InvalidOperationException("Revision obtaining must be called under exclusive lock");
-        
-      if (_curRev == _maxRev) {
-        _maxRev += _revCount;
-        WriteMaxRev();
-      }
-      
-      return ++_curRev;
+      var rev = Interlocked.Increment(ref _curRev);
+
+      if (rev >= _maxRev)
+      lock (typeof(Revision))
+        if (rev >= _maxRev)
+          WriteRev(_maxRev += _revCount);
+
+      return rev;
     }
  
-    private static void ReadCurRev()
+    private static long ReadRev()
     {
+      long result;
       using (var stream = File.Open(_revFilePath, FileMode.OpenOrCreate))
       using (var reader = new StreamReader(stream))
-        _curRev = long.TryParse(reader.ReadToEnd(), out _curRev) ? _curRev : 0; 
+        return long.TryParse(reader.ReadToEnd(), out result) ? result : 0; 
     }
     
-    private static void WriteMaxRev()
+    private static void WriteRev(long rev)
     {
       using (var stream = File.Open(_revFilePath, FileMode.Open))
       using (var writer = new StreamWriter(stream))
-        writer.Write(_maxRev.ToString());
+        writer.Write(rev.ToString());
     }
   }
 }
