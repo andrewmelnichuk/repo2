d8af0866fca8a7b19214c9509a5732698da1a6a5
andrewmelnichuk <andrew.melnichuk@gmail.com>
2015-08-26 18:42:10 +0300

    Logging test

--- Web/Server/Api/Sync/SyncController.cs
+++ Web/Server/Api/Sync/SyncController.cs
@@ -3,6 +3,7 @@ using Server.Common.Data;
 using Server.Common.Changes;
 using System.Collections.Generic;
 using Server.Common;
+using NLog;
 
 namespace Server.Api.Sync
 {
@@ -10,9 +11,12 @@ namespace Server.Api.Sync
   [Route("api/[controller]")]
   public class SyncController: Controller
   {
+    private static Logger logger = LogManager.GetCurrentClassLogger();
+    
     [HttpGet("index")]
     public List<Entity> Sync(long rev)
     {
+      logger.Info("sync");
       return ChangesManager.GetChanges(rev);
     }
   }
